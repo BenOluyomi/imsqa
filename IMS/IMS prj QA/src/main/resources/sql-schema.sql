cccd84deb1bcd2af34b01994782c56389973cd04
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-18 12:05:25 +0000

    working crud for customer(1)

--- IMS/IMS prj QA/src/main/resources/sql-schema.sql
+++ IMS/IMS prj QA/src/main/resources/sql-schema.sql
@@ -6,4 +6,13 @@ CREATE TABLE IF NOT EXISTS `customerorders`.`customers` (
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `last_name` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `customerorders`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL DEFAULT NULL,
+    `price` DECIMAL(10,2) NULL,
+    PRIMARY KEY (`item_id`)
+CREATE TABLE IF NOT EXISTS `customerorders`.`orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    PRIMARY KEY (`order_id`)
 );
