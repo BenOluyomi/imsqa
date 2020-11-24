5f13700b062f9010b7cc81e306c04ed2454656e5
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-24 12:43:24 +0000

    more classes tested, and total cost implemented

--- IMS/IMS prj QA/target/test-classes/sql-data.sql
+++ IMS/IMS prj QA/target/test-classes/sql-data.sql
@@ -9,3 +9,11 @@ INSERT INTO `customerorders`.`orders` (id, order_id) VALUES (2, 2);
 
 INSERT INTO `customerorders`.`order_items` (order_id, item_id, quantity) VALUES (1, 1, 3);
 INSERT INTO `customerorders`.`order_items` (order_id, item_id, quantity) VALUES (2, 2, 2);
+
+alter table order_items add cost decimal(10,2);
+
+
+update order_items set cost = ( select item_price from items where order_items.item_id = items.item_id);
+
+alter table order_items add total_cost decimal(10,2);
+update order_items set total_cost = quantity*cost;
